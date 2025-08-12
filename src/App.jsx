ef86576b62052ec3b8be3f24131f1144b447ed0d
Tia Piol <tiarox02@yahoo.com>
2025-08-12 10:44:15 -0700

    exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 // src/App.jsx
+import { Route, Routes } from "react-router";
+import NavBar from "./components/NavBar/NavBar";
+import { useState } from "react";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  return (
+  <>
+  <NavBar />
+  <h1>Mailboxes</h1>
+  </>
+  )
+}
 
 export default App;
 
